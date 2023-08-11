b26e0503e749059c486b903ba0f0ec78c1ec850d
Jorge <110149668+JorgeMRs@users.noreply.github.com>
2023-08-11 18:17:06 +0000

    Falta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,8 +8,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 const Home = () => {
 
-	const [caso, setCaso] = useState()
+
+
+
 	const fondo = { height: "100%", width: "100%" }
+	const [caso, setCaso] = useState()
+	const [id, setId] = useState("")
 	const [repl, setRepl] = useState("block")
 	const [pausel, setPausel] = useState("none")
 
@@ -28,28 +32,23 @@ const Home = () => {
 
 	}
 
-	let canci = cancion.map
-	console.log(canci)
-
 
 	function play() {
 
-		console.log(canci)
-
-		if (repl == "block") {
-
+		if (botoAudio.current.paused) {
 			botoAudio.current.play()
 			setRepl("none")
 			setPausel("block")
+
 		} else {
 			setPausel("none")
 			setRepl("block")
-
 			botoAudio.current.pause()
 
 		}
 
 	}
+
 	useEffect(function () {
 
 		obtenerInfo()
@@ -58,8 +57,44 @@ const Home = () => {
 
 
 	function sig() {
+		if (caso < cancion.length) {
+			setCaso(caso + 1)
+			imprimir(caso)
+			selecto.style.background = "red"
+			console.log("hola")
+
+		} else if (caso == cancion.length - 1) {
+			imprimir(caso)
+			selecto.style.background = "red"
+			setCaso(caso)
+
+
+
+
+		}
+	}
+
+	function back() {
+		if (caso > 0) {
+			setCaso(caso - 1)
+			imprimir(caso)
+		} else if (caso == 0) {
+			imprimir(caso)
+			setCaso(caso)
+
+
+		}
+	}
+
+	function imprimir(num) {
+		let ran = 'https://assets.breatheco.de/apis/sound/' + cancion[num].url
+		botoAudio.current.src = ran
+		console.log(botoAudio.current)
+		play()
 
 	}
+
+
 
 	return (
 		<div className="text-start" style={fondo}>
@@ -67,20 +102,21 @@ const Home = () => {
 				<div className="col-6 m-auto " >
 					<div className="overflow-auto" style={stylebotton} id="list-tab" role="tablist"  >
 						<div className="list-group" id="list-tab" role="tablist" style={stylebotton}  >
-							{cancion.map(function (item) { return <a className="col text-white bg-black p-3 list-group-item list-group-item-action" id="list-home-list" data-bs-toggle="list" role="tab" key={item.id} onClick={() => setUrl(item.url)}>{item.id + " " + item.name + " " + "-"}</a> })}
+							{cancion.map(function (item) { return <a className="col text-white  p-3 opacity-75 list-group-item list-group-item-action" id={item.id} data-bs-toggle="list" role="tab" key={item.id} onClick={() => { setUrl(item.url); setCaso(item.id), setId(item.id) }}>{item.id + " " + item.name + " " + "-"}</a> })}
 
 						</div>
 
 					</div>
 				</div>
 			</div>
+
 			<div className="bg-black col-6 m-auto p-4 d-flex justify-content-evenly">
 
 				<audio className="d-none" src={"https://assets.breatheco.de/apis/sound/" + url} ref={botoAudio}>
 				</audio>
 
-				<a className={"d-" + "block"} onClick={() => sig()} >
-					<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-left-square-fill" viewBox="0 0 16 16">
+				<a className="" onClick={() => back()} >
+					<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-caret-left-square-fill" viewBox="0 0 16 16">
 						<path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm10.5 10V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4A.5.5 0 0 0 10.5 12z" />
 					</svg>
 				</a>
@@ -88,20 +124,20 @@ const Home = () => {
 				<div className="PlayStop">
 
 					<a className={"d-" + repl} onClick={() => play()}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-play-circle-fill" viewBox="0 0 16 16">
 							<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
 						</svg>
 					</a>
 					<a className={"d-" + pausel} onClick={() => play()}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
+						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-pause-circle-fill" viewBox="0 0 16 16">
 							<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z" />
 						</svg>
 
 					</a>
 				</div>
 
-				<a className={"d-" + "block "} /* onClick={() =>play()} */>
-					<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-right-square-fill" viewBox="0 0 16 16">
+				<a className="" onClick={() => sig()} >
+					<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-caret-right-square-fill" viewBox="0 0 16 16">
 						<path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4v8z" />
 					</svg>
 				</a>
